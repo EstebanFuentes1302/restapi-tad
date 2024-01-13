3ac402bba53a769fd9e1ef2c0e8293746678ad62
Esteban Fuentes <esteban.fuentes@unmsm.edu.pe>
2024-01-13 16:44:40 -0500

    print port test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 
 //Variables
 app.set('port', process.env.PORT || 3000); //Valida si la variable PORT existe, sino es 3000
+console.log(process.env.PORT);
 
 //Middlewares
 app.use(morgan('dev')); //Para imprimir las solicitudes en consola - formato "dev"
